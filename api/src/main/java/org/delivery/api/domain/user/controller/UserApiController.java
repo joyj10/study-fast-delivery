41c6d98facf30c970f24874c2452ed23802d4eb6
dev_one <micool1030@gmail.com>
2024-03-25 06:50:49 +0900

    [refactor] UserSession 애노테이션 작업 - 토큰 유저 정보 기준으로 유저 정보 조회하는 애노테이션 작업 - UserResolver 작업

--- api/src/main/java/org/delivery/api/domain/user/controller/UserApiController.java
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserApiController.java
@@ -1,9 +1,11 @@
 package org.delivery.api.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.comon.annotation.UserSession;
 import org.delivery.api.comon.api.Api;
 import org.delivery.api.domain.user.business.UserBusiness;
 import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.delivery.api.domain.user.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +22,8 @@ public class UserApiController {
     private final UserBusiness userBusiness;
 
     @GetMapping("/me")
-    public Api<UserResponse> me() {
-        RequestAttributes requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
-        Long userId = Long.parseLong((String) Objects.requireNonNull(requestContext.getAttribute("userId", RequestAttributes.SCOPE_REQUEST)));
-        UserResponse response = userBusiness.me(userId);
+    public Api<UserResponse> me(@UserSession User user) {
+        UserResponse response = userBusiness.me(user.getId());
         return Api.OK(response);
     }
 }
