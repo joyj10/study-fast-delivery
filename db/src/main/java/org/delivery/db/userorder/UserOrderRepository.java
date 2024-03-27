fc53237944df95a933da45e8c5292bfba4fa9c6a
dev_one <micool1030@gmail.com>
2024-03-28 05:59:55 +0900

    [feat] 사용자 주문, 사용자 주문 메뉴 repository, service 개발 - 주문 조회, 상태 변경 작업

--- db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
+++ db/src/main/java/org/delivery/db/userorder/UserOrderRepository.java
@@ -1,6 +1,17 @@
 package org.delivery.db.userorder;
 
+import org.delivery.db.userorder.enums.UserOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserOrderRepository extends JpaRepository<UserOrderEntity, Long> {
+    // 특정 유저의 모든 주문
+    List<UserOrderEntity> findAllByUserIdAndStatusOrderByIdDesc(Long userId, UserOrderStatus status);
+
+    List<UserOrderEntity> findAllByUserIdAndStatusInOrderByIdDesc(Long userId, List<UserOrderStatus> statusList);
+
+    // 특정 주문
+    Optional<UserOrderEntity> findByIdAndStatusAndUserId(Long id, UserOrderStatus status, Long userId);
 }
--- db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
+++ db/src/main/java/org/delivery/db/userorder/enums/UserOrderStatus.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum UserOrderStatus {
+    REGISTERED("등록"),
+    UNREGISTERED("해지"),
     ORDER("주문"),
-    CANCEL("취소"),
+    ACCEPT("확인"),
+    COOKING("요리중"),
+    DELIVERY("배달중"),
+    RECEIVE("완료"),
     ;
 
     private final String description;
