3e4dcfb9848e6e460e2872011c9e64315ac8e6ee
dev_one <micool1030@gmail.com>
2024-03-31 12:16:59 +0900

    [feat] 로그인 개발 - 로그인 시 UserDetails 리턴하는 로직 개발

---
+++ store-admin/src/main/java/org/delivery/storeadmin/domain/authorization/AuthorizationService.java
@@ -0,0 +1,37 @@
+package org.delivery.storeadmin.domain.authorization;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.db.storeuser.StoreUserEntity;
+import org.delivery.storeadmin.domain.user.service.StoreUserService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final StoreUserService storeUserService;
+
+    /**
+     * 로그인 로직
+     *  1. 사용자가 로그인 시도를 하면 입력한 username 가지고 해당 코드 실행
+     *  2. 입력한 이메일 기준으로 사용자 검색
+     *  3. 값이 없는 경우 예외 발생
+     *  4. 값이 있는 경우 값을 담아서 리턴
+     *  5. 리턴된 UserDetails를 스프링 시큐리티가 패스워드를 꺼내서 사용자 입력 패스워드를 해시처리 한 다음 비교해서 로그인 처리
+     * */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        StoreUserEntity storeUserEntity = storeUserService.getRegisterUser(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return User.builder()
+                .username(storeUserEntity.getEmail())
+                .password(storeUserEntity.getPassword())
+                .roles(storeUserEntity.getRole().toString())
+                .build();
+    }
+}
